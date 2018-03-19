fb6f48faad9cf0ec50e5cceb23ba6b571132079d
Chely Ho <adept_inept@yahoo.com>
2018-03-19 23:30:02 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,10 +24,8 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug/edit' do
-    binding.params
     @song = Song.find_by_slug(params[:slug])
     @genres = Genre.all
-
     erb :'songs/edit'
   end
 
